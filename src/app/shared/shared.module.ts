7ee7a1d4b5a7999b6df3159941d1f7ffb3653680
Dusan Malusev <dusan.998@outlook.com>
2020-08-24 21:05:02 +0200

    Implementing url http interceptor
    Signed-off-by: Dusan Malusev <dusan.998@outlook.com>

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,24 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+import { UrlInterceptor } from './interceptors/url.interceptor';
 import { SubscriptionService } from './services/subscription.service';
 import { ContactService } from './services/contact.service';
+
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, FontAwesomeModule, HttpClientModule],
   exports: [FontAwesomeModule, HttpClientModule],
-  providers: [SubscriptionService, ContactService],
+  providers: [
+    SubscriptionService,
+    ContactService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: UrlInterceptor,
+      multi: true,
+    },
+  ],
 })
 export class SharedModule {}
